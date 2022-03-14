1447616c6228bda6b356a0982a786d89c7a90872
Duc Vinh Le <vinhxp95@gmail.com>
2022-03-14 21:33:40 +1100

    react-router-version 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
+import Header from './components/Home/Header';
 function App() {
   return (
-    <div className="App">
-        CHUONG 4
+    // Route phien ban 6. se khac voi 5. => video is on 5. verison
+    // Vi Header khong dat ben trong Routes nen Header se duoc duy tri xuyen suot cac pages 
+    <div>
+      {/* <Header></Header>  dat o day cung duoc */}
+      <BrowserRouter>
+      <Header></Header>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
+
+    /**
+     * <BrowserRouter>
+     * <Switch>
+     * <Route  exact path='/home' component={Home} />
+        <Route  exact path='/contact' component={Contact} />
+        <Route exact path='/about' component={About} />
+        <Route  exact path='/' component={Home} />
+     * </Switch>
+        
+    </BrowserRouter>
+     * 
+     */
+
   );
 }
 
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,9 +1,13 @@
+
+
 import {combineReducers, createStore} from 'redux';
 
-
+const state ={}
 const rootReducer = combineReducers({
     // state here
+    appState :state
 })
 
-export const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
+export default store;
